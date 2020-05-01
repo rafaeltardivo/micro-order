a74a08c67fae2f92c089934d6375570bd1be812e
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-01 16:02:52 -0300

    Add orders app configuration

--- services/orders/app/__init__.py
+++ services/orders/app/__init__.py
@@ -1,5 +1,19 @@
 import logging
-logFormatter = ('TIMESTAMP:%(asctime)s LEVEL:%(levelname)s MSG:%(message)s'
-                ' PAYLOAD:%(payload)s')
-logging.basicConfig(format=logFormatter, level=logging.INFO)
-logger = logging.getLogger(__name__)
+
+from decouple import config
+
+from .publishers import Publisher
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('orders')
+
+retries = config('CONNECTION_RETRIES', default=3, cast=int)
+wait_time = config('SECONDS_BETWEEN_RETRIES', default=2, cast=int)
+broker_host = config('BROKER_HOST', default='rabbit')
+
+publisher = Publisher(retries, wait_time, broker_host)
+if not publisher.connection:
+    logger.info('Could not connect to Broker!')
+    raise SystemExit
+else:
+    logger.info('Successfully connected to broker.')
--- services/orders/app/apps.py
+++ services/orders/app/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
+from . import logger
+
 
 class AppConfig(AppConfig):
     name = 'app'
+
+    def ready(self):
+        import app.signals  # noqa
