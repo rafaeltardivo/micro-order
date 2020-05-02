1139286e80e7406585dff089297b2cf3b062b6dd
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-02 16:59:31 -0300

    Add pubsub module

--- services/shipping/app/pubsub/callbacks.py
+++ services/shipping/app/pubsub/callbacks.py
@@ -1,19 +1,31 @@
 import json
 
-# from . import logger
+from app import logger, producer
+
+from app.models import Shipping
 
 def bind_orders_callback(channel, method, properties, payload):
-    """callback for bind_orders command."""
-    print('aaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-    order_payload = json.dumps(payload)
+    """callback for bind_orders command."""    
 
     try:
-        pass
-    except KeyError:
-        # logger.info("Failed to parse order")
-        pass
+        order = json.loads(payload)
+        customer = order['customer']
+        order_id = order['id']
+    except (TypeError, KeyError):
+        logger.info("Could not parse payload: {}.".format(payload))
     else:
-        pass
+        logger.info("Received order Payload: {}.".format(order))
+        new_shipping = Shipping.objects.create(order=order_id)
         
+        payload = {
+            'shipping': new_shipping.pk,
+            'customer': customer
+        }
+
+        producer.publish_to(
+            exchange='shippings',
+            routing_key='shippings_create',
+            payload=json.dumps(payload)
+        )
 
 
--- services/shipping/app/pubsub/producers.py
+++ services/shipping/app/pubsub/producers.py
@@ -1,5 +1,6 @@
 import pika
 
+from . import logger
 
 class Producer:
     __instance = None
@@ -21,20 +22,31 @@ class Producer:
             Producer.__instance = self
             Producer.__instance.connection = connection
             Producer.__instance.exchanges = dict()
-    
-    def publish_to(self, exchange, routing_key, payload):
-        """Channel Producer."""
-        channel = None
+
+    def declare_exchange(self, exchange):
+        """Declare and store exchange."""
 
         try:
-            channel = self.exchanges['exchange']
+            channel = self.exchanges[exchange]
+            logger.info('Found exchange {}'.format(exchange))
         except KeyError:
             channel = self.connection.channel()
             channel.exchange_declare(exchange=exchange)
-            self.exchanges['exchange'] = channel
+            self.exchanges[exchange] = channel
+            logger.info('Created exchange {}'.format(exchange))
+
+    def publish_to(self, exchange, routing_key, payload):
+        """Exchange Producer."""
+
+        try:
+            channel = self.exchanges[exchange]
+        except KeyError:
+            logger.info('Could not publish to {}'.format(exchange))
         finally:
             channel.basic_publish(
                 exchange=exchange,
                 routing_key=routing_key,
                 body=payload
             )
+            logger.info('Published {} to {}'.format(payload, exchange))
+
