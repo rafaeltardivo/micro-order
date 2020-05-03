55a957a4016f6f098400dc81bd42067a40f854f4
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-03 16:18:35 -0300

    Refactor consumer to include schemas

--- services/orders/app/pubsub/consumers.py
+++ services/orders/app/pubsub/consumers.py
@@ -1,22 +1,37 @@
-import pika
+from marshmallow import Schema, fields
 
 
 class Consumer:
     __instance = None
 
-    @staticmethod 
+    @staticmethod
     def get_instance(connection):
-        """Static access method."""
+        """Static access to Producer singleton.
+        Args:
+            connection (pika.BlockingConnection): BlockingConnection object.
+        Returns:
+            Consumer singleton.
+        """
 
-        if Consumer.__instance == None:
+        if Consumer.__instance is None:
             Consumer(connection)
             return Consumer.__instance
 
     def __init__(self, connection):
-        """Virtually private constructor."""
+        """Virtually private constructor for Producer singleton.
+        Args:
+            connection (pika.BlockingConnection): BlockingConnection object.
+        """
 
-        if Consumer.__instance != None:
-            raise Exception("This class is a Singleton!")
+        if Consumer.__instance is not None:
+            raise TypeError("This class is a Singleton!")
         else:
             Consumer.__instance = self
             Consumer.__instance.connection = connection
+            Consumer.__instance.shipping_update_schema = Schema.from_dict(
+                {
+                    'id': fields.Integer(),
+                    'order': fields.Integer(),
+                    'status': fields.Integer()
+                }
+            )
