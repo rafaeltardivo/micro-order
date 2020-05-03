31b764e81790965e1ffcf2eb25e4097ce0473681
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-03 15:19:15 -0300

    Update Orders view to ModelViewSet

--- services/orders/app/views.py
+++ services/orders/app/views.py
@@ -1,20 +1,23 @@
-from rest_framework.generics import CreateAPIView
-
-from .serializers import OrderSerializer
-from .models import Order
+from rest_framework.exceptions import MethodNotAllowed
+from rest_framework.viewsets import ModelViewSet
 
 from . import logger
+from .models import Order
+from .serializers import OrderSerializer
 
 
-class OrderCreateView(CreateAPIView):
-    """CreateView definition for Order."""
+class OrderViewSet(ModelViewSet):
+    """ModelViewSet definition for Order."""
+
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    
+
     def post(self, request, *args, **kwargs):
-        logger.info(
-            "Order create request with payload {}.".format(
-                request.data
-            )
-        )
+        logger.info("CREATE request: {}".format(request.data))
         return super().post(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        raise MethodNotAllowed(request.method)
+
+    def destroy(self, request, *args, **kwargs):
+        raise MethodNotAllowed(request.method)
