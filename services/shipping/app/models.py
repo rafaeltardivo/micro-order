b741fb60084656c1239aa59075fe34a4436ae0ac
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-03 00:34:45 -0300

    Update shippings model and migration

--- services/shipping/app/models.py
+++ services/shipping/app/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 class Shipping(models.Model):
     """Model definition for Shipping."""
+    PROCESSING = 1
+    SUCESS = 2
+    FAIL = 3
+
+    STATUS_CHOICES = [
+        (PROCESSING, "PROCESSING"),
+        (SUCESS, "SUCESS"),
+        (FAIL, "FAIL"),
+    ]
     order = models.PositiveIntegerField()
     shipped_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=PROCESSING)
 
     class Meta:
         verbose_name = 'shipping'
@@ -11,8 +21,9 @@ class Shipping(models.Model):
 
     def __str__(self):
         """Unicode representation of Shipping."""
-        return 'Shipping {} made at {} associated to order id {}'.format(
+        return 'Shipping {} order {} shipped_at {} status {}'.format(
             self.id,
+            self.order,
             self.shipped_at,
-            self.order
+            self.status
         )
