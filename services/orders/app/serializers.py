4aebfd25b42a73d64349e33061d87793f178c286
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-03 15:00:33 -0300

    Update Order serializer docstring

--- services/orders/app/serializers.py
+++ services/orders/app/serializers.py
@@ -1,24 +1,27 @@
 from django.core.exceptions import ValidationError
-
 from rest_framework import serializers
 
 from .models import Order
 
+
 class OrderSerializer(serializers.ModelSerializer):
     """Serializer definition for Order."""
-    status = serializers.CharField(source='get_status_display', read_only=True)
+
+    status = serializers.CharField(source="get_status_display", read_only=True)
 
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = "__all__"
         read_only = ("id", "made_at")
 
     def validate_customer(self, value):
-        """Customer id validation."""
+        """Customer id validation.
+        Args:
+            value (int): Customer id.
+        Returns:
+            value (int): Customer id.
+        """
 
         if value < 0:
             raise ValidationError("Must be greater then 0.")
         return value
-
-
-
