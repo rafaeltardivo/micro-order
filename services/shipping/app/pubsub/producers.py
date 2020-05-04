be61a7bb1643928908e1c3d96599a5368dbc2d1a
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-03 21:34:30 -0300

    Add producer schemas to Shipping Producer singleton

--- services/shipping/app/pubsub/producers.py
+++ services/shipping/app/pubsub/producers.py
@@ -1,30 +1,57 @@
-import pika
+from marshmallow import Schema, fields
 
 from . import logger
+
 
 class Producer:
     __instance = None
 
-    @staticmethod 
+    @staticmethod
     def get_instance(connection):
-        """Static access method."""
+        """Static access to Producer singleton.
+        Args:
+            connection (pika.BlockingConnection): BlockingConnection object.
+        Returns:
+            Producer singleton.
+        """
 
-        if Producer.__instance == None:
+        if Producer.__instance is None:
             Producer(connection)
             return Producer.__instance
 
     def __init__(self, connection):
-        """Virtually private constructor."""
+        """Virtually private constructor for Producer singleton.
+        Args:
+            connection (pika.BlockingConnection): BlockingConnection object.
+        """
 
-        if Producer.__instance != None:
-            raise Exception("This class is a Singleton!")
+        if Producer.__instance is not None:
+            raise TypeError("This class is a Singleton!")
         else:
             Producer.__instance = self
             Producer.__instance.connection = connection
             Producer.__instance.exchanges = dict()
+            Producer.__instance.customer_request_schema = Schema.from_dict(
+                {
+                    'id': fields.Integer(),
+                    'customer': fields.Integer()
+                }
+            )
+            Producer.__instance.shipping_update_schema = Schema.from_dict(
+                {
+                    'id': fields.Integer(),
+                    'order': fields.Integer(),
+                    'status': fields.Integer()
+                }
+            )
 
     def declare_exchange(self, exchange):
-        """Declare and store exchange."""
+        """Declare and store exchange.
+        Args:
+            exchange (str): Exchange name.
+        Returns:
+            None.
+        """
 
         try:
             channel = self.exchanges[exchange]
@@ -36,17 +63,23 @@ class Producer:
             logger.info('Created exchange {}'.format(exchange))
 
     def publish_to(self, exchange, routing_key, payload):
-        """Exchange Producer."""
+        """Publish payload to exchange.
+        Args:
+            exchange (str): Exchange name.
+            routing_key (str): routing key name for binding.
+            payload (json): routing key name for binding.
+        Returns:
+            None.
+        """
 
         try:
             channel = self.exchanges[exchange]
         except KeyError:
             logger.info('Could not publish to {}'.format(exchange))
-        finally:
+        else:
             channel.basic_publish(
                 exchange=exchange,
                 routing_key=routing_key,
                 body=payload
             )
-            logger.info('Published {} to {}'.format(payload, exchange))
-
+            logger.info('Published: {} to: {}'.format(payload, exchange))
