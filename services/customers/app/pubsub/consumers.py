731b4c9df37eee67a54bbe8ae373467dfc78ce55
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-03 22:04:54 -0300

    Update Customers pubsub Consumer format

---
+++ services/customers/app/pubsub/consumers.py
@@ -0,0 +1,36 @@
+from marshmallow import Schema, fields
+
+
+class Consumer:
+    __instance = None
+
+    @staticmethod
+    def get_instance(connection):
+        """Static access to Producer singleton.
+        Args:
+            connection (pika.BlockingConnection): BlockingConnection object.
+        Returns:
+            Consumer singleton.
+        """
+
+        if Consumer.__instance is None:
+            Consumer(connection)
+            return Consumer.__instance
+
+    def __init__(self, connection):
+        """Virtually private constructor for Producer singleton.
+        Args:
+            connection (pika.BlockingConnection): BlockingConnection object.
+        """
+
+        if Consumer.__instance is not None:
+            raise TypeError("This class is a Singleton!")
+        else:
+            Consumer.__instance = self
+            Consumer.__instance.connection = connection
+            Consumer.__instance.customer_request_schema = Schema.from_dict(
+                {
+                    'id': fields.Integer(),
+                    'customer': fields.Integer()
+                }
+            )
