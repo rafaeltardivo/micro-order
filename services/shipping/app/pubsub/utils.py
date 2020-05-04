6bd285dfd9809bfc459e75236e974b25e642faaa
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-03 21:33:36 -0300

    Add docstring to pubsub utils

--- services/shipping/app/pubsub/utils.py
+++ services/shipping/app/pubsub/utils.py
@@ -1,18 +1,25 @@
-import pika
 import time
 
-import decouple
+import pika
 
 
 def get_connection(retries, wait_time, broker_host):
-    """Broker connection utility."""
+    """Create broker connection.
+    Args:
+        retries (int): Maximum number of retries.
+        wait_time (int): Wait time (seconds) between retries.
+        broker_host (str): Broker host IP Address or hostname
+    Returns:
+        pika.BlockingConnection object.
+    """
     connection = None
 
     while(retries > 0):
         try:
             connection = pika.BlockingConnection(
-                parameters = pika.ConnectionParameters(
-                    host=broker_host)
+                parameters=pika.ConnectionParameters(
+                    host=broker_host
+                )
             )
         except pika.exceptions.AMQPConnectionError:
             retries -= 1
