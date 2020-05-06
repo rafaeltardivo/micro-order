be7f73f5f1d7e5a374b4ae089b16f5b2c4fef601
Rafael Domingues Tardivo <rafaeldt.br@gmail.com>
2020-05-06 14:44:41 -0300

    Add retry library

--- services/orders/app/pubsub/utils.py
+++ services/orders/app/pubsub/utils.py
@@ -1,9 +1,13 @@
-import time
-
 import pika
+from decouple import config
+from retry import retry
+
+retries = config('CONNECTION_RETRIES', default=3, cast=int)
+wait_time = config('SECONDS_BETWEEN_RETRIES', default=2, cast=int)
 
 
-def get_connection(retries, wait_time, broker_host):
+@retry(pika.exceptions.AMQPConnectionError, tries=retries, delay=wait_time)
+def get_connection(broker_host):
     """Create broker connection.
     Args:
         retries (int): Maximum number of retries.
@@ -12,18 +16,11 @@ def get_connection(retries, wait_time, broker_host):
     Returns:
         pika.BlockingConnection object.
     """
-    connection = None
 
-    while(retries > 0):
-        try:
-            connection = pika.BlockingConnection(
-                parameters=pika.ConnectionParameters(
-                    host=broker_host
-                )
-            )
-        except pika.exceptions.AMQPConnectionError:
-            retries -= 1
-            time.sleep(wait_time)
-        else:
-            break
+    connection = pika.BlockingConnection(
+        parameters=pika.ConnectionParameters(
+            host=broker_host
+        )
+    )
+
     return connection
