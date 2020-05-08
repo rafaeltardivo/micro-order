a27aa99f6bf9e8c22da65511890456e383a8ae37
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-08 20:37:52 -0300

    Add customers view mapping

--- services/customers/app/urls.py
+++ services/customers/app/urls.py
@@ -3,5 +3,5 @@ from rest_framework import routers
 from .views import CustomerViewSet
 
 router = routers.SimpleRouter()
-router.register("", CustomerViewSet, base_name="shippings")
+router.register("", CustomerViewSet, base_name="customers")
 urlpatterns = router.urls
