05d664cd11ccc1b5176e9b7f9c257a97c3fe5ac5
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-11 14:51:45 -0300

    Refactor customer callbacks logs

--- services/customers/app/pubsub/callbacks.py
+++ services/customers/app/pubsub/callbacks.py
@@ -1,6 +1,7 @@
-from app import logger, producer
+from app import producer
 from app.models import Customer
 
+from . import logger
 from .schemas import (customer_detail_schema, customer_request_schema,
                       customer_shipping_schema)
 
@@ -15,16 +16,15 @@ def customers_request_callback(channel, method, properties, payload):
     Returns:
         None.
     """
-
     shipping = customer_request_schema().loads(payload)
-    logger.info("Received shipping customer request payload: {}.".format(
-        shipping)
-    )
+    logger.info(f'Received shipping customer request payload: {shipping}')
     try:
         customer = Customer.objects.get(id=shipping['customer'])
     except Customer.DoesNotExist:
+        logger.info(f"Could not find customer: {shipping['customer']}")
         shipping['customer'] = {}
     else:
+        logger.info(f'Retrieved customer: {customer}')
         shipping['customer'] = customer_detail_schema().dump(customer)
     finally:
         shipping_payload = customer_shipping_schema().dumps(shipping)
