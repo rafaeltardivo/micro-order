b97b47fa97d4c0844d6cfe05f97e1d40ac838557
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-10 21:15:16 -0300

    Remove serializer date formating

--- services/shipping/app/serializers.py
+++ services/shipping/app/serializers.py
@@ -6,7 +6,6 @@ from .models import Shipping
 class ShippingSerializer(serializers.ModelSerializer):
     """Serializer definition for Shipping."""
     status = serializers.CharField(source="get_status_display", read_only=True)
-    shipped_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
 
     class Meta:
         model = Shipping
