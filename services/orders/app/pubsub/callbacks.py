b88818e79a577afbdcfd0c075d2744b6a2e45ab4
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-12 00:00:49 -0300

    Add shipping pubsub tests

--- services/orders/app/pubsub/callbacks.py
+++ services/orders/app/pubsub/callbacks.py
@@ -15,17 +15,14 @@ def shippings_update_callback(channel, method, properties, payload):
         None.
     """
 
-    if payload:
-        shipping_payload = shipping_update_schema().loads(payload)
-        logger.info(f'Received shipping update payload: {shipping_payload}')
-        try:
-            order = Order.objects.get(id=shipping_payload['order'])
-        except Order.DoesNotExist:
-            logger.info(f"Could not find order: {shipping_payload['order']}")
-        else:
-            logger.info(f'Retrieved order: {order}')
-            order.status = shipping_payload['status']
-            order.save()
-            logger.info(f'Updated order: {order.pk}')
+    shipping_payload = shipping_update_schema().loads(payload)
+    logger.info(f'Received shipping update payload: {shipping_payload}')
+    try:
+        order = Order.objects.get(id=shipping_payload['order'])
+    except Order.DoesNotExist:
+        logger.info(f"Could not find order: {shipping_payload['order']}")
     else:
-        logger.info("Missing shipping_update payload")
+        logger.info(f'Retrieved order: {order}')
+        order.status = shipping_payload['status']
+        order.save()
+        logger.info(f'Updated order: {order.pk}')
