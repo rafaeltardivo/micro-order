211547e557656f6a37b829769a40d6f68640b336
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-05-22 18:24:58 -0300

    Update shippings view class

--- services/shipping/app/views.py
+++ services/shipping/app/views.py
@@ -1,20 +1,17 @@
-from rest_framework.exceptions import MethodNotAllowed
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import (CreateModelMixin, ListModelMixin,
+                                   RetrieveModelMixin)
+from rest_framework.viewsets import GenericViewSet
 
 from .models import Shipping
 from .serializers import ShippingSerializer
 
 
-class ShippingViewSet(ModelViewSet):
-    """ModelViewSet definition for Shipping."""
+class ShippingViewSet(
+    GenericViewSet,
+    CreateModelMixin,
+    ListModelMixin,
+    RetrieveModelMixin,
+):
+    """Create/List/Detail view for Shipping."""
     queryset = Shipping.objects.all()
     serializer_class = ShippingSerializer
-
-    def post(self, request, *args, **kwargs):
-        raise MethodNotAllowed(request.method)
-
-    def update(self, request, *args, **kwargs):
-        raise MethodNotAllowed(request.method)
-
-    def destroy(self, request, *args, **kwargs):
-        raise MethodNotAllowed(request.method)
